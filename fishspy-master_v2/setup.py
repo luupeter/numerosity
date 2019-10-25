6d67307829bdc76ea5725c82777a9cb022a8f242
luupeter <43586640+luupeter@users.noreply.github.com>
2019-10-25 14:30:27 -0700

    Add files via upload

---
+++ fishspy-master_v2/setup.py
@@ -0,0 +1,30 @@
+
+#
+# Copyright 2015 University of Southern California
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+#
+
+from distutils.core import setup
+
+setup(
+    name="fishspy",
+    description="zebrafish video analysis",
+    version="20191009.0",
+    scripts=[
+        "bin/fishspy-analyze-movie",
+        "bin/fishspy-plot-results",
+        "bin/fishspy-plot-cohort",
+        "bin/fishspy_worker"
+    ],
+    requires=["numpy", "scipy"],
+    maintainer_email="support@misd.isi.edu",
+    license='(new) BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+    ])
